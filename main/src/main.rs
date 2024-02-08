9a2d01022c4e10fd163169603a9d69f4600fd16c
Hugo Strappazzon <hugo.strapp@hotmail.fr>
2024-02-08 17:59:53 +0100

    TLU transformation

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,11 +1,12 @@
-use petgraph::{prelude::*, visit::GetAdjacencyMatrix};
+use itertools::Itertools;
+use petgraph::{data::Build, graph, prelude::*, visit::{GetAdjacencyMatrix, EdgeCount}};
 
 const INF: i32 = 99999999;
 
 struct SPTree {
-    t: Vec<(u32, u32)>,
-    l: Vec<(u32, u32)>,
-    u: Vec<(u32, u32)>,
+    t: Vec<(usize, usize)>,
+    /*    l: Vec<(usize, usize)>,
+    u: Vec<(usize, usize)>,*/
 }
 
 fn main() {
@@ -22,62 +23,71 @@ fn main() {
     graph.add_edge(n1, t, 1);
     graph.add_edge(root, t, 1);
 
-    let adj = graph.adjacency_matrix();
+    //let adj = graph.adjacency_matrix();
 
     //Flow capacities and costs
-    //let c = arr2(&[[0, 1, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1, 1, 0]]);
-    let N = 4;
-    let g = PRstruct{
-        n : N, 
-        capacity: Vec::from([
-        0i32, 2, 3, 0,
-        0, 0, 1, 3,
-        0, 0, 0, 2,
-        0, 0, 0, 0,
-    ]),
-        flow: vec![0; 4*4],
+    let c = Vec::from([0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0]);
+    let u = Vec::from([0i32, 2, 3, 0, 0, 0, 1, 3, 0, 0, 0, 2, 0, 0, 0, 0]);
+
+    //Maxflow
+    initialization(graph, u, c);
+}
+
+fn initialization<'a, N: 'a, E>(graph: DiGraph<N, E>, u: Vec<i32>, c: Vec<i32>) -> SPTree {
+    let N = graph.node_count();
+    let g = PRstruct {
+        n: N,
+        capacity: u.clone(),
+        flow: vec![0; N * N],
         label: Vec::new(),
         excess: Vec::new(),
         excess_vertices: Vec::new(),
         seen: Vec::new(),
     };
-    
-    println!("{:?}", g.capacity);
-    //Maxflow
-    let feasible_sol:Vec<i32> = max_flow(0, N-1, g);
-    
 
+    //Supposing source_id = 0 and sink_id = lastnode id
+    let source_id = 0;
+    let sink_id = graph.node_count() - 1;
 
-    println!("{:?}", feasible_sol);
+    let initial_feasible_flow = max_flow(source_id, sink_id, g);
 
+    //Filling T arc-set with free arc of the initial feasible solution
+    let mut freearcs = graph.edge_references().filter(|&x| {
+        initial_feasible_flow[x.source().index() * N + x.target().index()] > 0
+            && initial_feasible_flow[x.source().index() * N + x.target().index()]
+                < u[x.source().index() * N + x.target().index()]
+    });
 
-    //initialization(graph, x, u, c);
+    let T: Vec<(usize, usize)> = freearcs
+        .map(|x| (x.source().index(), x.target().index()))
+        .collect();
+
+    /*  Idea here :
+        while !is_tree(T) {
+            search arc;
+            T.push(arc);
+        }
+    */
+    is_tree(T.clone(), N);
+
+    let TLU = SPTree { t: T };
+    println!("{:?}", TLU.t);
+    TLU
 }
 
+fn is_tree(edges: Vec<(usize, usize)>, n: usize) -> bool {
+    let mut graph = Graph::<(), ()>::new();
 
-
-/*
-fn _initialization<N, E>(
-    graph: DiGraph<N, E>,
-    x: Array2<u32>,
-    u: Array2<u32>,
-    c: Array2<u32>,
-) -> SPTree {
-    
-
-
-
-
-
-
-
-    let mut T: Vec<(u32, u32)> = Vec::new();
-    let mut L: Vec<(u32, u32)> = Vec::new();
-    let mut U: Vec<(u32, u32)> = Vec::new();
-
-    SPTree { t: T, l: L, u: U }
-}*/
-
+    let (mut left, right): (Vec<_>, Vec<_>) = edges.clone().into_iter().unzip();
+    left.extend(right);
+    println!("{:?}", left);
+ /*   let nodes: Vec<usize> = left.into_iter().dedup().collect();
+    nodes.iter().for_each(|&x| {graph.add_node(());});*/
+    let addedge:Vec<(u32, u32)> = edges.iter().map(|x| (x.0 as u32, x.1 as u32)).collect();
+    graph.extend_with_edges(addedge);
+    //println!("{:?}", petgraph::algo::bellman_ford(graph, 0));
+    true
+}
 
 ///////////////////////////
 //Push-Relabel algorithm
@@ -85,8 +95,8 @@ fn _initialization<N, E>(
 
 //structure for Push-Relabel algorithm
 struct PRstruct {
-    capacity: Vec<i32>,//2d vec flatten
-    flow: Vec<i32>,//2d vec flatten
+    capacity: Vec<i32>, //2d vec flatten
+    flow: Vec<i32>,     //2d vec flatten
     label: Vec<i32>,
     excess: Vec<i32>,
     excess_vertices: Vec<usize>,
@@ -95,9 +105,9 @@ struct PRstruct {
 }
 
 fn push(u: usize, v: usize, mut g: PRstruct) -> PRstruct {
-    let d = std::cmp::min(g.excess[u], g.capacity[g.n*u+v] - g.flow[g.n*u+v]);
-    g.flow[g.n*u+v] += d;
-    g.flow[g.n*v+u] -= d;
+    let d = std::cmp::min(g.excess[u], g.capacity[g.n * u + v] - g.flow[g.n * u + v]);
+    g.flow[g.n * u + v] += d;
+    g.flow[g.n * v + u] -= d;
     g.excess[u] -= d;
     g.excess[v] += d;
     if d != 0 && g.excess[v] == d {
@@ -109,7 +119,7 @@ fn push(u: usize, v: usize, mut g: PRstruct) -> PRstruct {
 fn relabel(u: usize, mut g: PRstruct) -> PRstruct {
     let mut d = INF;
     for i in 0..g.n {
-        if (g.capacity[g.n*u+i] - g.flow[g.n*u+i]) > 0 {
+        if (g.capacity[g.n * u + i] - g.flow[g.n * u + i]) > 0 {
             d = std::cmp::min(d, g.label[i]);
         }
     }
@@ -123,7 +133,7 @@ fn discharge(u: usize, mut g: PRstruct) -> PRstruct {
     while g.excess[u] > 0 {
         if g.seen[u] < g.n {
             let v = g.seen[u];
-            if (g.capacity[g.n*u+v] - g.flow[g.n*u+v]) > 0 && g.label[u] > g.label[v] {
+            if (g.capacity[g.n * u + v] - g.flow[g.n * u + v]) > 0 && g.label[u] > g.label[v] {
                 g = push(u, v, g);
             } else {
                 g.seen[u] += 1;
@@ -139,7 +149,7 @@ fn discharge(u: usize, mut g: PRstruct) -> PRstruct {
 fn max_flow(s: usize, t: usize, mut g: PRstruct) -> Vec<i32> {
     g.label = vec![0; g.n];
     g.label[s] = g.n as i32;
-    g.flow = vec![0; g.n*g.n];
+    g.flow = vec![0; g.n * g.n];
     g.excess = vec![0; g.n];
     g.excess[s] = INF;
     for i in 0..g.n {
@@ -157,8 +167,8 @@ fn max_flow(s: usize, t: usize, mut g: PRstruct) -> Vec<i32> {
     }
     let mut maxflow = 0;
     for i in 0..g.n {
-        maxflow += g.flow[i*g.n + t];
+        maxflow += g.flow[i * g.n + t];
     }
     println!(" MAX FLOW : {:?}", maxflow);
-    g.flow.iter().map(|&x| if x < 0 {0} else {x}).collect()
+    g.flow.iter().map(|&x| if x < 0 { 0 } else { x }).collect()
 }
---
+++ main/target/debug/deps/either-451eec4af3f4220a.d
@@ -0,0 +1,5 @@
+/home/hstrappa/Parallel_simplex_network/main/target/debug/deps/libeither-451eec4af3f4220a.rmeta: /home/hstrappa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/either-1.9.0/src/lib.rs
+
+/home/hstrappa/Parallel_simplex_network/main/target/debug/deps/either-451eec4af3f4220a.d: /home/hstrappa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/either-1.9.0/src/lib.rs
+
+/home/hstrappa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/either-1.9.0/src/lib.rs:
