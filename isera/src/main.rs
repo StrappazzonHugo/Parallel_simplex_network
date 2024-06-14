6632d8356ef4245fdc3e3251a02e782091e4b8a3
Hugo Strappazzon <hugo.strapp@hotmail.fr>
2024-06-14 16:01:37 +0200

    clear repo + new instances + added trait pivot rules

--- isera/src/main.rs
+++ isera/src/main.rs
@@ -1,24 +1,24 @@
-use isera::min_cost;
+use isera::{min_cost, PivotRules};
 use std::time::SystemTime;
 
-use crate::parser::parsed_graph;
-mod parser;
+use crate::dimacs_parser::parsed_graph;
+mod dimacs_parser;
 
 fn main() {
     //let args: Vec<String> = std::env::args().collect();
     //let mut rng = rand::thread_rng();
 
-    let (graph, sources, sinks) = parsed_graph();
+    let (graph, sources, sinks) = parsed_graph::<i64>();
 
     let start = SystemTime::now();
     println!(
-        "node nb = {:?}, edge nb = {:?}",// sources = {:?}, sinks = {:?}",
+        "node nb = {:?}, edge nb = {:?}",// sources = {:?}, sinks = {:?},
         graph.node_count(),
         graph.edge_count(),
         //sources,
         //sinks
     );
-    let _min_cost_flow = min_cost(graph, sources, sinks);
+    let _min_cost_flow = min_cost(graph, sources, sinks, PivotRules::BlockSearch, 1);
     match start.elapsed() {
         Ok(elapsed) => {
             println!("time = {:?}", (elapsed.as_millis() as f64 / 1000f64) as f64);
