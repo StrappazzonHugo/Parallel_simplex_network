1f87df569982fc229ccfcf3f1a697c5f45ea9a6a
Hugo Strappazzon <hugo.strapp@hotmail.fr>
2024-07-12 13:35:16 +0200

    modif bench script

--- isera/bench.sh
+++ isera/bench.sh
@@ -2,17 +2,14 @@
 
 cargo build --release
 
-FILES="$1/*.min"
-for f in $FILES
-do
-  ./target/release/isera $f -k 1 -n 1 >> res.csv 
-  ./target/release/isera $f -k 1 -n 2 >> res.csv 
-  ./target/release/isera $f -k 2 -n 2 >> res.csv 
-  ./target/release/isera $f -k 2 -n 4 >> res.csv 
-  ./target/release/isera $f -k 4 -n 4 >> res.csv 
-  ./target/release/isera $f -k 4 -n 8 >> res.csv 
-  ./target/release/isera $f -k 8 -n 8 >> res.csv 
-done
+FILES=$1
+./target/release/isera -f $FILES -k 1 -n 1 >> res.csv 
+./target/release/isera -f $FILES -k 1 -n 2 >> res.csv 
+./target/release/isera -f $FILES -k 2 -n 2 >> res.csv 
+./target/release/isera -f $FILES -k 2 -n 4 >> res.csv 
+./target/release/isera -f $FILES -k 4 -n 4 >> res.csv 
+./target/release/isera -f $FILES -k 4 -n 8 >> res.csv 
+./target/release/isera -f $FILES -k 8 -n 8 >> res.csv 
 
 
 
